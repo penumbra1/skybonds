17b8b7f2aadb56a8e49f4f27dc4695454d4e8df9
Olga Isakova <penumbral@yandex.ru>
2019-03-13 21:35:54 +0300

    Limit data variation

--- src/generator.js
+++ src/generator.js
@@ -1,32 +1,48 @@
 const getRandom = (from, to) => {
-  return Math.floor((Math.random() * (to - from) + Math.min(from, 0)) * 100) / 100;
-}
+  return (
+    Math.floor((Math.random() * (to - from) + Math.min(from, 0)) * 100) / 100
+  );
+};
 
-const getDataForDay = () => {
+const getInitialData = () => {
   return {
     price: getRandom(10, 100),
-    yield: getRandom(0, 8),
+    currentYield: getRandom(0, 8),
     spread: getRandom(-5, 5)
-  }
-}
+  };
+};
+
+const simulateChange = (n, delta) =>
+  getRandom(Math.floor(n - delta), Math.floor(n + delta));
+
+const getNextData = ({ price, currentYield, spread }) => {
+  return {
+    price: simulateChange(price, 5),
+    currentYield: simulateChange(currentYield, 1),
+    spread: simulateChange(spread, 2)
+  };
+};
 
 const getRating = () => {
   const letter = "ABC"[Math.floor(Math.random() * 3)];
-  return letter.repeat(1 + Math.floor(Math.random() * 3))
-}
+  return letter.repeat(1 + Math.floor(Math.random() * 3));
+};
 
-const MAX_DAYS = 10;
+const MAX_DAYS = 365;
 const generateDataPoints = () => {
   const dataPoints = [];
   let date = new Date();
-  date = date.setDate(date.getDate() - MAX_DAYS);
-  for (let i = 0; i < MAX_DAYS; i++) {
-    dataPoints.push({date, quotes: getDataForDay()});
-    date = date.setDate(date.getDate() + 1);
+  date.setDate(date.getDate() - MAX_DAYS + 1);
+
+  let dailyData = getInitialData();
+
+  for (let i = 0; i < MAX_DAYS; i++, date.setDate(date.getDate() + 1)) {
+    dataPoints.push({ date: new Date(date), ...dailyData });
+    dailyData = getNextData(dailyData);
   }
 
   return dataPoints;
-}
+};
 
 const generateResponse = isin => {
   return {
@@ -34,8 +50,11 @@ const generateResponse = isin => {
     name: "Some Company",
     issuer: "Some Company LLC.",
     rating: getRating(),
+    coupon: 5.55,
     data: generateDataPoints()
-  }
-}
+  };
+};
 
-console.log(JSON.stringify(generateResponse("ABCDE12345"), null, 2));
+// console.log(JSON.stringify(generateResponse("YYY"), null, 2));
+
+export default generateResponse;
