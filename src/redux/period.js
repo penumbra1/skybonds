25573a35f23444e669244fb8899506cda633f15e
Olga Isakova <penumbral@yandex.ru>
2019-03-14 18:49:32 +0300

    getPeriodInDays

--- src/redux/period.js
+++ src/redux/period.js
@@ -1,3 +1,5 @@
+import moment from "moment";
+
 const SET_PERIOD = "skybonds/period/SET_PERIOD";
 
 export function setPeriod(period) {
@@ -11,6 +13,29 @@ export const periods = {
   YEAR: "YEAR",
   MAX: "MAX"
 };
+
+export function getPeriodInDays(state, untilDate) {
+  let subtractArgs = [];
+  switch (state) {
+    case periods.WEEK:
+      subtractArgs = [1, "week"];
+      break;
+    case periods.MONTH:
+      subtractArgs = [1, "month"];
+      break;
+    case periods.QUARTER:
+      subtractArgs = [3, "months"];
+      break;
+    case periods.YEAR:
+      subtractArgs = [1, "year"];
+      break;
+    default:
+      return state;
+  }
+  const until = moment(untilDate);
+  const periodInDays = until.diff(until.subtract(...subtractArgs), "days");
+  return periodInDays;
+}
 
 export default function reducer(state = periods.WEEK, action = {}) {
   switch (action.type) {
