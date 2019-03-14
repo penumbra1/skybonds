df341cd1b0f20ba712fc9079dfc0094748d4329e
Olga Isakova <penumbral@yandex.ru>
2019-03-15 00:20:59 +0300

    ChartCard responsive styles

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import React, { Component } from "react";
 import Chart from "./components/Chart";
-import Layout from "./components/Layout";
+import ChartCard from "./components/ChartCard";
 import IndicatorSelect from "./components/IndicatorSelect";
 import PeriodSelect from "./components/PeriodSelect";
-import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <Layout>
-        <Chart />
-        <IndicatorSelect />
-        <PeriodSelect />
-      </Layout>
+      <div className="App">
+        <ChartCard>
+          <Chart />
+          <IndicatorSelect />
+          <PeriodSelect />
+        </ChartCard>
+      </div>
     );
   }
 }
--- src/components/ChartCard.js
+++ src/components/ChartCard.js
@@ -1,32 +1,36 @@
 import React from "react";
 import { connect } from "react-redux";
 import moment from "moment";
-import { Card, Statistic, Row, Col } from "antd";
+import { Card, Typography, Statistic, Row, Col } from "antd";
 
 const { Meta } = Card;
-const MetaItem = ({ children }) => <span className="meta">{children}</span>;
 
 const ChartCard = ({ name, issuer, coupon, maturity, children }) => (
   <Card
     title={
-      <Meta
-        title={name.toUpperCase()}
-        description={
-          <>
-            <p>{issuer}</p>
-            <Statistic
-              title="Coupon rate"
-              value={coupon}
-              precision={2}
-              suffix="%"
-            />
-            <Statistic
-              title="Maturity"
-              value={moment(maturity).format("DD.MM.YY")}
-            />
-          </>
-        }
-      />
+      <Row gutter={16} align="center" type="flex">
+        <Col span={12}>
+          <Meta
+            title={name.toUpperCase()}
+            description={
+              <Typography.Paragraph ellipsis style={{ margin: 0 }}>
+                {issuer}
+              </Typography.Paragraph>
+            }
+            style={{ margin: 0 }}
+          />
+        </Col>
+        <Col span={6}>
+          <Statistic title="Coupon" value={coupon} precision={2} suffix="%" />
+        </Col>
+        <Col span={6}>
+          <Statistic
+            title="Maturity"
+            value={moment(maturity).format("YYYY")}
+            groupSeparator=""
+          />
+        </Col>
+      </Row>
     }
     style={{ maxWidth: "95vw" }}
   >
