cf01baaf66b39f1f674ed7bcde4b8e33aa71f88d
Olga Isakova <penumbral@yandex.ru>
2019-03-15 06:35:45 +0300

    Default state, thunk

--- src/redux/data.js
+++ src/redux/data.js
@@ -1,14 +1,40 @@
-const GET_DATA = "skybonds/data/GET_DATA";
+import generateResponse from "../generator";
 
-export function getData(isin) {
-  // Should be a thunk that calls the data generator
-  return { type: GET_DATA, isin };
+const SET_DATA = "skybonds/data/GET_DATA";
+const SET_ISIN = "skybonds/data/SET_ISIN";
+
+export function setIsin(isin) {
+  return { type: SET_ISIN, isin };
 }
 
-export default function reducer(state = {}, action = {}) {
+export function setData(data) {
+  return { type: SET_DATA, data };
+}
+
+export function fetchData(isin) {
+  return function(dispatch) {
+    const data = generateResponse(isin);
+    dispatch(setData(data));
+  };
+}
+
+const defaultState = {
+  isin: "ABCDEF123456",
+  name: "",
+  issuer: "",
+  currency: "",
+  rating: "",
+  coupon: 0,
+  maturity: "",
+  dataPoints: []
+};
+
+export default function reducer(state = defaultState, action = {}) {
   switch (action.type) {
-    case GET_DATA:
-      return state;
+    case SET_ISIN:
+      return { ...state, isin: action.isin };
+    case SET_DATA:
+      return action.data;
     default:
       return state;
   }
