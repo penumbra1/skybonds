9761085a7c7fc0146204f8b1eadcd88fe670a7bb
Olga Isakova <penumbral@yandex.ru>
2019-03-15 11:28:30 +0300

    Bundle optimizations

--- config-overrides.js
+++ config-overrides.js
@@ -1,9 +1,22 @@
-const { override, fixBabelImports } = require("customize-cra");
+const {
+  override,
+  fixBabelImports,
+  addWebpackAlias,
+  addBabelPlugin
+} = require("customize-cra");
+const path = require("path");
 
 module.exports = override(
-  fixBabelImports("import", {
-    libraryName: "antd",
+  // Antd tree-shaking
+  fixBabelImports("antd", {
     libraryDirectory: "es",
     style: "css"
+  }),
+  // ~ -200Kb without the webpack entry
+  addBabelPlugin("recharts"),
+  // Antd icon bundle fix
+  // https://github.com/ant-design/ant-design/issues/12011#issuecomment-423173228
+  addWebpackAlias({
+    "@ant-design/icons/lib/dist$": path.resolve(__dirname, "./src/icons.js")
   })
 );
---
+++ src/icons.js
@@ -0,0 +1,7 @@
+// https://github.com/timarney/react-app-rewired/issues/167#issuecomment-350999274
+export {
+  default as DownOutline
+} from "@ant-design/icons/lib/outline/DownOutline";
+export {
+  default as SearchOutline
+} from "@ant-design/icons/lib/outline/SearchOutline";
